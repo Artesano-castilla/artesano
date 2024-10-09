419062474eca2cf2f3d15b839f9cf541e82c0648
ricardo <ricardo.garcia@gba.gob.ar>
2024-10-09 05:19:53 -0300

    fix ancla menu

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Selecciona todos los enlaces del menú lateral
+const sidebarLinks = document.querySelectorAll('#sidebar .list-items li a');
+const checkbox = document.getElementById('btn'); // El checkbox que controla el menú lateral
+
+// Función para cerrar el menú y navegar al ancla
+sidebarLinks.forEach(link => {
+  link.addEventListener('click', function() {
+    checkbox.checked = false; // Cierra el menú lateral
+  });
+});
